8e34eaa5073cc1a8fdf82911432e3c734a561574
Harrison Knapp <125601731+hknapp518@users.noreply.github.com>
2026-01-28 13:21:06 -0500

    Create Get-HardwareInventory.ps1

---
+++ powershell/Get-HardwareInventory.ps1
@@ -0,0 +1,19 @@
+#This script uses PowerShell and Common Information Model CIM to collect standardized hardware metadata device name, manufacturer, model, and serial number which can be used for asset tracking, lifecycle planning, and reporting within Intune.
+
+# Get-HardwareInventory.ps1
+# Collects basic hardware information for lifecycle tracking
+
+$ComputerSystem = Get-CimInstance -ClassName Win32_ComputerSystem
+$BIOS = Get-CimInstance -ClassName Win32_BIOS
+
+$HardwareInventory = [PSCustomObject]@{
+    DeviceName    = $env:COMPUTERNAME
+    Manufacturer  = $ComputerSystem.Manufacturer
+    Model         = $ComputerSystem.Model
+    SerialNumber  = $BIOS.SerialNumber
+    CollectedDate = (Get-Date).ToString("yyyy-MM-dd")
+}
+
+$HardwareInventory
+
+ 
